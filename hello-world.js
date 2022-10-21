e546ad4c9f31e9f3cf9c1873fcf5a66c0fe244a6
Mariana Pinto <mariana.vendasbb@gmail.com>
2022-10-21 14:10:13 -0300

    rendering and creating an element

---
+++ hello-world.js
@@ -0,0 +1,5 @@
+ReactDOM.render(
+  //atualiza os elementos DOM (nossa tela)
+  React.createElement("h1", "null", "Hello World"), // Criando um elemento para ser injetado
+  document.getElementById("root") // Injetando o elemento no root
+);
