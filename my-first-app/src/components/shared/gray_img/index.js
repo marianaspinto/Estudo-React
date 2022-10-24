b1ba20b6fb5131738553fe52333cb42411d7e049
Mariana Pinto <mariana.vendasbb@gmail.com>
2022-10-24 12:41:30 -0300

    creation of the organo app + first componente - main banner

---
+++ my-first-app/src/components/shared/gray_img/index.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import "./style.css";
+const GrayImg = () => {
+  return (
+    <Fragment>
+      <h3>Planet List</h3>
+      <hr />
+      <Planet />
+    </Fragment>
+  );
+};
+
+export default Planets;
---
+++ organo/src/componentes/Banner/Banner.js
@@ -0,0 +1,6 @@
+function Banner() {
+  // JSX
+  return <img src="/images/banner.png" alt="Banner principal"></img>;
+}
+
+export default Banner;
