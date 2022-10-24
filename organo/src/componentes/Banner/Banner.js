4cd1653a5f32c2505f0b26c18b54a01be5b6d1fd
Mariana Pinto <mariana.vendasbb@gmail.com>
2022-10-24 14:18:23 -0300

    text field creation + component parameters

--- organo/src/componentes/Banner/Banner.js
+++ organo/src/componentes/Banner/Banner.js
@@ -1,12 +1,10 @@
 import "./banner.css";
 
-function Banner() {
+export const Banner = () => {
   // JSX
   return (
     <header className="banner">
       <img src="/images/banner.png" alt="Banner principal"></img>;
     </header>
   );
-}
-
-export default Banner;
+};
